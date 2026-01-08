573a0920ab6c4dff1fcb9320bc5b023e70d61581
Dayer <dayerxr.cher@gmail.com>
2026-01-08 13:00:59 +0800

    fix: debugged issue of local database not syncing

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 
-import { pool } from './pool.js';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config()
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS members(
@@ -9,23 +11,33 @@ CREATE TABLE IF NOT EXISTS members(
     name VARCHAR ( 255 ),
     email VARCHAR ( 255 ),
     password VARCHAR ( 255 )
-)
+);
 
-CREATE TABLE IF NOT EXISTS message_tracker(
+CREATE TABLE IF NOT EXISTS messages(
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR ( 255 ),
     email VARCHAR ( 255 ),
     message VARCHAR ( 255 )
-)
+);
+
+INSERT INTO members(name, email, password) VALUES
+    ('Raphael Ho Zi Jie', 'raphaelhozj@gmail.com', '676767');
+
+INSERT INTO messages(name, email, message) VALUES
+    ('Raphael Ho Zi Jie', 'raphaelhozj@gmail.com', 'I goon to Arsernal');
 
 `
 
 async function DatabaseLoader(){
   console.log("Populating database via script");
 
-  await pool.connect();
-  await pool.query(SQL);
-  await pool.end();
+  const ConnectionString = process.env.LOCAL_DATABASE_URL
+  const client = new Client({
+      connectionString: ConnectionString
+  })
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
   console.log("Done");
 }
 
