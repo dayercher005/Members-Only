f4e6ac1877154c703237c873102d4018204edd73
Dayer <dayerxr.cher@gmail.com>
2026-01-08 15:55:22 +0800

    fix: debugged passport middleware not authenticating

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { IndexRouter } from './src/routes/MessagesPage/index.js';
 import { SignUpFormRouter } from './src/routes/Sign-Up/sign-up.js';
 import { LogInFormRouter } from './src/routes/Log-In/log-in.js';
+import { LogOutRouter } from './src/routes/Log-Out/log-out.js';
+import { ErrorRouter } from './src/routes/Error/error.js';
+import { pool } from './src/db/pool.js';
+import passport from 'passport';
+import session from 'express-session';
+import LocalStrategy from 'passport-local';
+
 
 const app = express();
 
@@ -14,9 +22,51 @@ app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }))
+app.use(passport.session())
+
+passport.use(
+    new LocalStrategy( async (email, password, done) => {
+        try{
+            const { rows } = await pool.query('SELECT * FROM members WHERE email = $1', [email])
+            const member = rows[0];
+
+            if (!member){
+                return done(null, false, {message: "Incorrect Username"})
+            }
+
+            if (member.password !== password){
+                return done(null, false, {message: "Incorrect password"})
+            }
+            return done(null, member);
+
+        } catch(error){
+            return done(error)
+        }
+    })
+)
+
+passport.serializeUser((member, done) => {
+    done(null, member.id);
+})
+
+passport.deserializeUser( async (id, done) => {
+    try{
+        const { rows } = await pool.query('SELECT * FROM members WHERE id = $1', [id])
+        const member = rows[0];
+        done(null, member);
+    } catch(error){
+        done(error)
+    }
+})
+
+
+app.use("/", IndexRouter);
 app.use("/sign-up", SignUpFormRouter);
 app.use("/log-in", LogInFormRouter);
+app.use("/log-out", LogOutRouter);
+app.use("/error", ErrorRouter);
 
 
 const PORT = 8080;
