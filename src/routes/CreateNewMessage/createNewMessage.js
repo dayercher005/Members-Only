9c756fea4cd815eff1e2e61ed654e1be27bac03c
Dayer <dayerxr.cher@gmail.com>
2026-01-12 21:05:56 +0800

    feat: adding routes for admin form

--- src/routes/CreateNewMessage/createNewMessage.js
+++ src/routes/CreateNewMessage/createNewMessage.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 import { renderNewMessageForm, sendNewMessageForm } from '../../controllers/CreateNewMessage/createNewMessage.js';
+import { ensureAuthentication } from '../../config/passport.js';
 
 
 export const CreateNewMessageRouter = Router();
 
-function ensureAuthentication(request, response, next){
-    if (request.isAuthenticated()){
-        return next();
-    }
-    response.redirect('/log-in');
-}
 
 CreateNewMessageRouter.get("/", ensureAuthentication , renderNewMessageForm);
 CreateNewMessageRouter.post("/", sendNewMessageForm);
