76b8ae9511fef4157cd18217e74ba0520dfa88d8
Dayer <dayerxr.cher@gmail.com>
2026-01-13 10:33:50 +0800

    refactor: refactored app.js file

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }))
 app.use(passport.session())
 
