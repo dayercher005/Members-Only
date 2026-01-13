a39d31d2cd0735d0ce2a3fa6eb0b2adcec150e04
Dayer <dayerxr.cher@gmail.com>
2026-01-13 13:32:09 +0800

    fix: fix issue of home page not syncing with membership column in database

--- src/routes/MessagesPage/index.js
+++ src/routes/MessagesPage/index.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { renderIndexPage } from '../../controllers/MessagesPage/renderIndex.js'
+import { renderIndexPage } from '../../controllers/MessagesPage/renderIndex.js';
+import { renderIndividualMessages } from '../../controllers/MessagesPage/renderIndividualMessages.js';
+import { ensureAuthentication } from '../../config/passport.js'
 
 export const IndexRouter = Router();
 
