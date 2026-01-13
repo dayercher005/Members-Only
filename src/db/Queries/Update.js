a2b4c2e3465164ec350c727664368f29afd5cacd
Dayer <dayerxr.cher@gmail.com>
2026-01-13 13:30:37 +0800

    fix: fix query for updating membership status

--- src/db/Queries/Update.js
+++ src/db/Queries/Update.js
@@ -1,8 +1,6 @@
 import { pool } from '../pool.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export async function UpdateMembership(admin, id){
-    await pool.query('UPDATE members SET membership = $1 WHERE id = $2', [admin, id])
+    console.log(id);
+    await pool.query('UPDATE members SET membership = $1 WHERE id = $2', [admin, id]);
 }
